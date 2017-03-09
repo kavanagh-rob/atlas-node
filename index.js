ef61e65936c31075d25c832d006b9965a5f7f98b
Rob <kavanagh.m.rob@gmail.com>
2017-03-09 00:08:53 +0000

    update racecard

--- index.js
+++ index.js
@@ -5,28 +5,37 @@ var finishedLoading = false;
 var racecardUrl = "http://www.sportinglife.com/racing/racecards";
 var resultsUrl = "http://www.sportinglife.com/racing/fast-results";
 
-//getRacecards(racecardUrl);
-getResults(resultsUrl);
+getRacecards(racecardUrl);
+//getResults(resultsUrl);
 
 function getRacecards(url){
     request(url, function (error, response, html) {
         if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(html);
-              var meetingSections = $('.rac-dgp');
-            meetingSections.each(function(i, raceMeetingSectionElement){
-                    var courseName = $(raceMeetingSectionElement).find('h3 a').text().replace(/\s/g,'');
-                console.log(courseName)
-                    if(isRelevantCourse(courseName)){
-                        console.log(courseName);
-                        var racesArray = [];
-                         $(raceMeetingSectionElement).find('.rac-cards').each(function(i, raceElement){
-                            var race = {};
-                            race.raceTime = $(raceElement).find('.ixt').text();
-                            race.raceName = $(raceElement).find('.ixc').text().trim();
-                            racesArray[i] = race;
-                        });   
-                    }  
-            });
+            var $ = cheerio.load(html);var racesArray = [];
+            var racesMeetingsArray = [];
+            var meetingIndex = 0;
+              var meetingSections = $('.hr-meeting-container');
+                meetingSections.each(function(i, meetingSection){
+                        var courseName = $(meetingSection).find('.dividerRow h2').text();
+                        if(isRelevantCourse(courseName)){
+                            var racesArray = [];
+                             $(meetingSection).find('li').each(function(i, raceElement){
+                                var race = {};
+                                race.time = $(raceElement).find('.hr-meeting-race-time').text();            
+                                race.name = $(raceElement).find('.hr-meeting-race-name-star').text().trim();
+                                 if( $(raceElement).find('.hr-meeting-race-result-fulllink-btn').attr('href')){
+                                     race.resultLink = $(raceElement).find('.hr-meeting-race-result-fulllink-btn').attr('href').trim();
+                                 }
+                                racesArray[i] = race;
+                            });
+                            var meeting = {};
+                            meeting.name = courseName;
+                            meeting.races = racesArray;
+                            racesMeetingsArray[meetingIndex] = meeting;
+                            meetingIndex++;
+                        }  
+                });
+            console.log(racesMeetingsArray);
         }
     });
 }
@@ -36,35 +45,44 @@ function getResults(url){
         if (!error && response.statusCode == 200) {
             var $ = cheerio.load(html);
             var resultList = [];
+            var resultsIndex = 0;
               var fastResults = $('.fast-results-list .fast-results-item');
                 fastResults.each(function(i, resultElement){
                         var result = {};
-                
-                        result.raceCourse = $(resultElement).find('.fast-result-header-race').text().trim();
-                        result.raceTime = $(resultElement).find('.time-short').text().trim();
-                        result.racePositions = [];
-                        $(resultElement).find('.fast-results-place').each(function(i, horseElement){
-                            var horse = {};
-                                horse.position = $(horseElement).find('.fast-results-place-position').text().trim();
-                            console.log($(horseElement).find('.fast-results-place-position').text().trim());
-//                                horse.number = $(horseElement).find('td')[1].children[0].data.trim();
-//                                horse.odds = $(horseElement).find('td')[2].children[0].data.trim();
-//                                horse.name =  $(resultElement).find('a').text();
-                             result.racePositions[i] = horse;
-                        });     
-                    resultList[i] = result; 
-                });       
+                        var parts = $(resultElement).find('.fast-result-header-race').text().trim().split(" ");
+                        result.raceCourse = parts[1];
+                        if(parts.length >2){
+                            result.raceCourse += parts[2];
+                        }
+                        if(isRelevantCourse(result.raceCourse)){
+                            result.raceTime = $(resultElement).find('.time-short').text().trim();
+                            result.racePositions = [];
+                            $(resultElement).find('.fast-results-place').each(function(i, horseElement){
+                                var horse = {};
+                                    horse.position = $(horseElement).find('.fast-results-place-position').text().trim();
+                                    horse.number = $(horseElement).find('.fast-results-place-number').text().trim();
+                                    horse.name = $(horseElement).find('.fast-results-place-name').text().trim();
+                                    horse.odds =  $(horseElement).find('.fast-results-place-odds').text().trim();
+                                 result.racePositions[i] = horse;
+                            });     
+                        resultList[resultsIndex] = result; 
+                        resultsIndex++;
+                        }
+                       
+                }); 
+            console.log(resultList);
         }
     });
 }
 
-function isRelevantCourse(courseName){
+function isRelevantCourse(courseName){  
     return britishRacecourses.indexOf(courseName) != -1 || irishRacecourses.indexOf(courseName) != -1 ;    
 }
 
 
 var britishRacecourses = [
-    "Aintree",  
+    "MahoningValley",
+    "Aintree", 
     "Ascot",  
     "Ayr",   
     "Bath",  
@@ -74,26 +92,33 @@ var britishRacecourses = [
     "Cartmel",  
     "Catterick",  
     "Cheltenham",  
-    "ChelmsfordCity",  
+    "ChelmsfordCity", 
+    "Chelmsford", 
     "Chepstow",  
     "Chester",  
     "Doncaster",  
-    "EpsomDowns",  
+    "EpsomDowns",
+    "Epsom",
     "Exeter",  
     "Fakenham",  
     "FfosLas",  
-    "FontwellPark",  
+    "FontwellPark",
+    "Fontwell",
     "Goodwood",  
     "GreatYarmouth",  
-    "HamiltonPark",  
+    "HamiltonPark",
+    "Hamilton",
     "HaydockPark",  
+    "Haydock",  
     "Hereford",  
     "Hexham",  
     "Huntingdon",  
     "Kelso",  
-    "KemptonPark",  
+    "KemptonPark", 
+    "Kempton", 
     "Leicester",  
     "LingfieldPark",  
+    "Lingfield",  
     "Ludlow",  
     "MarketRasen",  
     "Musselburgh",  
@@ -108,7 +133,8 @@ var britishRacecourses = [
     "Redcar",  
     "Ripon",  
     "Salisbury",  
-    "SandownPark",  
+    "SandownPark",
+    "Sandown",
     "Sedgefield",  
     "Southwell",   
     "Taunton",  
